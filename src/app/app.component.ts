13b397be37d9e9a4fdba49836c6cdf8b59e20863
Michał Jaskulski <jaskulskim.gdynia@gmail.com>
2023-12-25 02:08:05 +0100

    Login init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { NavComponent } from './components/nav/nav.component'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { DescriptionComponent } from './components/desctiption/description.component'
-import { HomePageComponent } from './components/views/home-page/home-page.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    NgbModule,
-    NavComponent,
-    FontAwesomeModule,
-    DescriptionComponent,
-    HomePageComponent,
-  ],
+  imports: [CommonModule, RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'listenly'
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomePageComponent } from './components/views/home-page/home-page.component'
+import { LoginPageComponent } from './components/views/login-page/login-page.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Listenly | Home Page',
+    component: HomePageComponent,
+  },
+  {
+    path: 'login',
+    title: 'Listenly | Sign in',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'songs',
+    title: 'Listenly | Songs',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'albums',
+    title: 'Listenly | Albums',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'playlists',
+    title: 'Listenly | Playlists',
+    component: LoginPageComponent,
+  },
+]
